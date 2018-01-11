b6ce12e8efa0da1f06cf87cd0c13f02b1aeaf227
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-01-11 15:23:59 +0100

    moved everything to main

--- JJJWebServer/JJJWebServer/Program.cs
+++ JJJWebServer/JJJWebServer/Program.cs
@@ -4,15 +4,85 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace JJJWebServer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] prefixes)
         {
-        string[] prefixes = {"http://localhost:8080/"};
-        TestWebServer ws = new TestWebServer(prefixes);
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            // URI prefixes are required,
+            // for example "http://localhost:8080/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prefixes.
+            listener.Prefixes.Add("http://localhost:8080/");
+            
+            listener.Start();
+            Console.WriteLine("Listening...");
+            
+            while (listener.IsListening)
+            {
+                string path = @"...\...\...\...\Content";
+                // Note: The GetContext method blocks while waiting for a request. 
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                //GetContentTypeStuff(context);
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                byte[] buffer = File.ReadAllBytes(path + "\\" + request.RawUrl);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+            }
+            
+            listener.Stop();
         }
+
+        public static void GetContentTypeStuff(HttpListenerContext context)
+        {
+            if (Path.GetExtension(context.Request.RawUrl) == ".jpg")
+            {
+                context.Response.ContentType = "image/jpeg";
+            }
+            else if (Path.GetExtension(context.Request.RawUrl) == ".gif")
+            {
+                context.Response.ContentType = "image/gif";
+            }
+            else if (Path.GetExtension(context.Request.RawUrl) == ".css")
+            {
+                context.Response.ContentType = "text/css";
+            }
+            else if (Path.GetExtension(context.Request.RawUrl) == ".htm")
+            {
+                context.Response.ContentType = "text/html";
+            }
+            else if (Path.GetExtension(context.Request.RawUrl) == ".html")
+            {
+                context.Response.ContentType = "text/html";
+            }
+            else if (Path.GetExtension(context.Request.RawUrl) == ".ico")
+            {
+                context.Response.ContentType = "image/x-icon";
+            }
+            else if (Path.GetExtension(context.Request.RawUrl) == ".js")
+            {
+                context.Response.ContentType = "application/x-javascript";
+            }
+        }
+        
     }
 }
