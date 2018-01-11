1f9d17d242a92108c79cde7ce13b4d1af34580ea
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-01-11 09:50:04 +0100

    Added TestWebServer Class

---
+++ JJJWebServer/JJJWebServer/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJJWebServer
+{
+    public class TestWebServer
+    {
+        private HttpListener listener = new HttpListener();
+        private Func<HttpListenerRequest, string> responseMethod;
+        public TestWebServer(string[] prefixes, Func<HttpListenerRequest, string> webHost) 
+        {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException(
+                    "Needs Windows XP SP2, Server 2003 or later.");
+
+            // URI prefixes are required, for example 
+            // "http://localhost:8080/index/"
+            if (prefixes == null || prefixes.Length == 0)
+            {
+                throw new ArgumentException("prefixes");
+            }
+
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            //webHost cannot = null
+            responseMethod = webHost ?? throw new ArgumentException("address");
+            listener.Start();
+
+            Console.WriteLine("Listening...");
+            // Note: The GetContext method blocks while waiting for a request. 
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            listener.Stop();
+        }
+
+    }
+}
--- JJJWebServer/JJJWebServer/Program.cs
+++ JJJWebServer/JJJWebServer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,7 @@ namespace JJJWebServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hejsan kompisar, JOCKE HÄR");
-            //
+            
         }
     }
 }
